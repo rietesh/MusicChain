14b8abde3585fbc589425ab9c13b875c2b0f2352
Rietesh <rietesh4535@iitg.ernet.in>
2020-08-06 21:32:41 +0530

    Update irohaBUY.py

--- flask/irohaBUY.py
+++ flask/irohaBUY.py
@@ -3,19 +3,19 @@ from iroha import IrohaCrypto
 from iroha import Iroha, IrohaGrpc
 
 
-IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '0.0.0.0')
+IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '35.240.140.120')
 IROHA_PORT = os.getenv('IROHA_PORT', '50051')
-ADMIN_ACCOUNT_ID = os.getenv('ADMIN_ACCOUNT_ID', 'admin@test')
+ADMIN_ACCOUNT_ID = os.getenv('ADMIN_ACCOUNT_ID', 'admin@mchain')
 ADMIN_PRIVATE_KEY = os.getenv(
-    'ADMIN_PRIVATE_KEY', 'f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70')
+    'ADMIN_PRIVATE_KEY', '505e3ee6da65ad07268236e49a302d982c027e353ee9bd0acb89b2c395151deb')
 
 
 def instantiate(src,dest,amt,priv_key):
-    iroha = Iroha(src+'@test')
+    iroha = Iroha(src+'@mchain')
     net = IrohaGrpc('{}:{}'.format(IROHA_HOST_ADDR, IROHA_PORT))
     tx = iroha.transaction([
-        iroha.command('TransferAsset', src_account_id=src+'@test', dest_account_id=dest+'@test',
-                      asset_id='coin#test', amount=amt)
+        iroha.command('TransferAsset', src_account_id=src+'@mchain', dest_account_id=dest+'@mchain',
+                      asset_id='coin#mchain', amount=amt)
     ])
     priv = priv_key
     IrohaCrypto.sign_transaction(tx, priv)
@@ -23,7 +23,7 @@ def instantiate(src,dest,amt,priv_key):
 
 
 def createAsset(asset_name,domain_id,precision,priv_key,name):
-    iroha = Iroha(name + '@test')
+    iroha = Iroha(name + '@mchain')
     net = IrohaGrpc('{}:{}'.format(IROHA_HOST_ADDR, IROHA_PORT))
     tx = iroha.transaction([
         iroha.command('CreateAsset', asset_name=asset_name, domain_id=domain_id, precision=precision)])
